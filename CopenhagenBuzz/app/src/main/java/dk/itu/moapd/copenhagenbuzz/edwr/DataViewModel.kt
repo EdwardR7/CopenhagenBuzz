0f26ebdf03ea321f8699f261efcff535c0a8971e
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2024-03-01 16:32:01 +0100

    exercise 5.2. DataViewModel

---
+++ CopenhagenBuzz/app/src/main/java/dk/itu/moapd/copenhagenbuzz/edwr/DataViewModel.kt
@@ -0,0 +1,42 @@
+package dk.itu.moapd.copenhagenbuzz.edwr
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dk.itu.moapd.copenhagenbuzz.edwr.Event
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class DataViewModel : ViewModel() {
+
+    // LiveData object to hold a list of events
+    private val _events = MutableLiveData<List<Event>>()
+
+    // Expose the list of events as LiveData
+    val events: LiveData<List<Event>>
+        get() = _events
+
+    // Method to fetch the list of events asynchronously
+    fun fetchEvents() {
+        CoroutineScope(Dispatchers.IO).launch {
+            // Generate or fetch the list of events asynchronously
+            val eventsList = generateOrFetchEvents()
+            // Update LiveData with the fetched events list
+            _events.postValue(eventsList)
+        }
+    }
+
+    // Method to generate or fetch the list of events asynchronously
+    private suspend fun generateOrFetchEvents(): List<Event> {
+        // Implement logic to generate or fetch the list of events
+        // For example, you can make a network request here
+        // For now, I'll provide mock data
+        return listOf(
+            Event("Event 1",  "Location 1", 1,"Type 1", "Description 1"),
+            Event("Event 2",  "Location 2", 2,"Type 2", "Description 2"),
+            Event("Event 3",  "Location 3", 3, "Type 3","Description 3")
+            // Add more events as needed
+        )
+    }
+}

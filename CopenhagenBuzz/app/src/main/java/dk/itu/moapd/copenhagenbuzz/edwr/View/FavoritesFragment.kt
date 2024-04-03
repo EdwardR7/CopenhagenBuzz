d8f1ac80b16d8ccfdecb010f916305c35726a63b
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2024-04-03 18:16:46 +0200

    bug

--- CopenhagenBuzz/app/src/main/java/dk/itu/moapd/copenhagenbuzz/edwr/View/FavoritesFragment.kt
+++ CopenhagenBuzz/app/src/main/java/dk/itu/moapd/copenhagenbuzz/edwr/View/FavoritesFragment.kt
@@ -1,10 +1,12 @@
+package dk.itu.moapd.copenhagenbuzz.edwr.View
+
+import FavoriteAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase

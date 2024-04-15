401834bd5badc152f5780554d4f31176b9919824
edward.rostomian@gmail.com <36159443+EdwardR7@users.noreply.github.com>
2024-04-15 21:49:22 +0200

    9.02 done ig?

--- CopenhagenBuzz/app/src/main/java/dk/itu/moapd/copenhagenbuzz/edwr/View/Fragment/MapsFragment.kt
+++ CopenhagenBuzz/app/src/main/java/dk/itu/moapd/copenhagenbuzz/edwr/View/Fragment/MapsFragment.kt
@@ -1,10 +1,13 @@
 package dk.itu.moapd.copenhagenbuzz.edwr.View.Fragment
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import dk.itu.moapd.copenhagenbuzz.edwr.databinding.FragmentMapsBinding
 
 class MapsFragment : Fragment() {
@@ -13,12 +16,31 @@ class MapsFragment : Fragment() {
         get() = requireNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
+    companion object {
+        const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = FragmentMapsBinding.inflate(inflater, container, false).also {
         _binding = it
     }.root
+
+    private fun checkPermission() =
+        ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestUserPermissions() {
+        if (!checkPermission())
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
+            )
+    }
+
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null

d87ac45c00d0da5efa4c021fe4d7f27e34815da6
Jerome <jrah@itu.dk>
2024-05-07 10:33:06 +0200

    Favorite fragment har også billeder nu

--- CopenhagenBuzz/app/src/main/java/dk/itu/moapd/copenhagenbuzz/edwr/Adapter/FavoriteAdapter.kt
+++ CopenhagenBuzz/app/src/main/java/dk/itu/moapd/copenhagenbuzz/edwr/Adapter/FavoriteAdapter.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.squareup.picasso.Picasso
 import dk.itu.moapd.copenhagenbuzz.edwr.Model.Event
 import dk.itu.moapd.copenhagenbuzz.edwr.R
 
@@ -30,11 +32,23 @@ class FavoriteAdapter(
     inner class ViewHolder(recyclerView: View) : RecyclerView.ViewHolder(recyclerView) {
         private val titleTextView: TextView = itemView.findViewById(R.id.text_event_title)
         private val typeTextView: TextView = itemView.findViewById(R.id.text_event_type)
+        private val imageView: ImageView = itemView.findViewById(R.id.image_event)
+
 
         fun bind(event: Event) {
             titleTextView.text = event.eventName
             typeTextView.text = event.eventType
             // Bind other event data here
+
+            // Inside populateView method
+            if (event.imageUrl.isNotEmpty()) {
+                // If imageUrl is not empty, load the image using Picasso
+                Picasso.get().load(event.imageUrl).into(imageView)
+            } else {
+                // If imageUrl is empty, set a placeholder image or hide the ImageView
+                imageView.setImageResource(R.drawable.baseline_image_24)
+            }
         }
+
     }
 }
